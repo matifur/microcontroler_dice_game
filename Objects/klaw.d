2fae522e63b47e5ed54303b888bfc6b1b2b4f62a
Mateusz Furgała <74820050+matifur@users.noreply.github.com>
2024-03-26 17:13:24 +0100

    Add files via upload

---
+++ Objects/klaw.d
@@ -0,0 +1,9 @@
+.\objects\klaw.o: ..\..\lab3\1_Klawiatura\klaw.c
+.\objects\klaw.o: ..\..\lab3\1_Klawiatura\klaw.h
+.\objects\klaw.o: C:\Users\matifur\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\MKL05Z4.h
+.\objects\klaw.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\klaw.o: D:\Program_Files\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\klaw.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\klaw.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\klaw.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\klaw.o: C:\Users\matifur\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\system_MKL05Z4.h
---
+++ Objects/main.d
@@ -0,0 +1,16 @@
+.\objects\main.o: main.c
+.\objects\main.o: frdm_bsp.h
+.\objects\main.o: C:\Users\matifur\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\MKL05Z4.h
+.\objects\main.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\main.o: D:\Program_Files\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Users\matifur\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Users\matifur\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\system_MKL05Z4.h
+.\objects\main.o: i2c.h
+.\objects\main.o: ADC.h
+.\objects\main.o: lcd1602.h
+.\objects\main.o: klaw.h
+.\objects\main.o: D:\Program_Files\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\main.o: D:\Program_Files\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\main.o: D:\Program_Files\Keil\ARM\ARMCC\Bin\..\include\time.h
